0a0c7a4783aafc195e3956861adceae673ae0a8d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WindowsFormsApplication1/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿Namespace WindowsFormsApplication1
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+            Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+            Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+            Me.btnFromOutlook = New DevExpress.XtraEditors.SimpleButton()
+            Me.simpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+            Me.dateNavigator1 = New DevExpress.XtraScheduler.DateNavigator()
+            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.dateNavigator1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' schedulerControl1
+            ' 
+            Me.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Month
+            Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.schedulerControl1.Location = New System.Drawing.Point(0, 77)
+            Me.schedulerControl1.Name = "schedulerControl1"
+            Me.schedulerControl1.Size = New System.Drawing.Size(509, 439)
+            Me.schedulerControl1.Start = New Date(2014, 12, 21, 0, 0, 0, 0)
+            Me.schedulerControl1.Storage = Me.schedulerStorage1
+            Me.schedulerControl1.TabIndex = 0
+            Me.schedulerControl1.Text = "schedulerControl1"
+            Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+            Me.schedulerControl1.Views.FullWeekView.Enabled = True
+            Me.schedulerControl1.Views.GanttView.Enabled = False
+            Me.schedulerControl1.Views.WeekView.Enabled = False
+            Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+            ' 
+            ' btnFromOutlook
+            ' 
+            Me.btnFromOutlook.Dock = System.Windows.Forms.DockStyle.Top
+            Me.btnFromOutlook.Location = New System.Drawing.Point(0, 0)
+            Me.btnFromOutlook.Name = "btnFromOutlook"
+            Me.btnFromOutlook.Size = New System.Drawing.Size(688, 39)
+            Me.btnFromOutlook.TabIndex = 1
+            Me.btnFromOutlook.Text = "Generate Holiday Appointments from Outlook.hol file"
+            ' 
+            ' simpleButton2
+            ' 
+            Me.simpleButton2.Dock = System.Windows.Forms.DockStyle.Top
+            Me.simpleButton2.Location = New System.Drawing.Point(0, 39)
+            Me.simpleButton2.Name = "simpleButton2"
+            Me.simpleButton2.Size = New System.Drawing.Size(688, 38)
+            Me.simpleButton2.TabIndex = 2
+            Me.simpleButton2.Text = "Generate Holiday Appointments from XML file"
+            ' 
+            ' dateNavigator1
+            ' 
+            Me.dateNavigator1.Dock = System.Windows.Forms.DockStyle.Right
+            Me.dateNavigator1.HighlightTodayCell = DevExpress.Utils.DefaultBoolean.Default
+            Me.dateNavigator1.HotDate = Nothing
+            Me.dateNavigator1.Location = New System.Drawing.Point(509, 77)
+            Me.dateNavigator1.Name = "dateNavigator1"
+            Me.dateNavigator1.SchedulerControl = Me.schedulerControl1
+            Me.dateNavigator1.Size = New System.Drawing.Size(179, 439)
+            Me.dateNavigator1.TabIndex = 3
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(688, 516)
+            Me.Controls.Add(Me.schedulerControl1)
+            Me.Controls.Add(Me.dateNavigator1)
+            Me.Controls.Add(Me.simpleButton2)
+            Me.Controls.Add(Me.btnFromOutlook)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.dateNavigator1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+        Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+        Private WithEvents btnFromOutlook As DevExpress.XtraEditors.SimpleButton
+        Private WithEvents simpleButton2 As DevExpress.XtraEditors.SimpleButton
+        Private dateNavigator1 As DevExpress.XtraScheduler.DateNavigator
+    End Class
+End Namespace
+
