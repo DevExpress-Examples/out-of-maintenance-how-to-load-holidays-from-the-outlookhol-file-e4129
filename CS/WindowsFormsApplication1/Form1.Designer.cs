ee319e05bde00ee0a25e9ccbc0a074ca6665275e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApplication1 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.btnFromOutlook = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.dateNavigator1 = new DevExpress.XtraScheduler.DateNavigator();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Month;
+            this.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.schedulerControl1.Location = new System.Drawing.Point(0, 77);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(509, 439);
+            this.schedulerControl1.Start = new System.DateTime(2014, 12, 21, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            this.schedulerControl1.Views.FullWeekView.Enabled = true;
+            this.schedulerControl1.Views.GanttView.Enabled = false;
+            this.schedulerControl1.Views.WeekView.Enabled = false;
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            // 
+            // btnFromOutlook
+            // 
+            this.btnFromOutlook.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFromOutlook.Location = new System.Drawing.Point(0, 0);
+            this.btnFromOutlook.Name = "btnFromOutlook";
+            this.btnFromOutlook.Size = new System.Drawing.Size(688, 39);
+            this.btnFromOutlook.TabIndex = 1;
+            this.btnFromOutlook.Text = "Generate Holiday Appointments from Outlook.hol file";
+            this.btnFromOutlook.Click += new System.EventHandler(this.btnFromOutlook_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.simpleButton2.Location = new System.Drawing.Point(0, 39);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(688, 38);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "Generate Holiday Appointments from XML file";
+            this.simpleButton2.Click += new System.EventHandler(this.btnFromXml_Click);
+            // 
+            // dateNavigator1
+            // 
+            this.dateNavigator1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dateNavigator1.HighlightTodayCell = DevExpress.Utils.DefaultBoolean.Default;
+            this.dateNavigator1.Location = new System.Drawing.Point(509, 77);
+            this.dateNavigator1.Name = "dateNavigator1";
+            this.dateNavigator1.CellPadding = new System.Windows.Forms.Padding(2);
+            this.dateNavigator1.SchedulerControl = this.schedulerControl1;
+            this.dateNavigator1.Size = new System.Drawing.Size(179, 439);
+            this.dateNavigator1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 516);
+            this.Controls.Add(this.schedulerControl1);
+            this.Controls.Add(this.dateNavigator1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.btnFromOutlook);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateNavigator1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.XtraEditors.SimpleButton btnFromOutlook;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraScheduler.DateNavigator dateNavigator1;
+    }
+}
+
